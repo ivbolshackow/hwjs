fb259adb788024b0f0f5bda4694d271d015d4d30
Иван Большаков <ivanbolsakov@MacBook-Pro-Ivan.local>
2022-06-22 13:58:16 +0300

    lesson-7

---
+++ 3 уровень/1-tabs/app.js
@@ -0,0 +1,61 @@
+'use strict';
+const texts = {
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
+    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+
+let title = document.querySelector('.text');
+
+let navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(function (link) {
+    link.addEventListener('click', clickHandler);
+});
+
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event 
+ */
+// здесь вызывайте changeText и changeActiveClass, и передавайте
+// им объект события.
+function clickHandler(event) {
+    changeText(event);
+    changeActiveClass(event);
+}
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event 
+ */
+function changeActiveClass(event) {
+    document.querySelector('.active').classList.remove('active');
+    event.target.classList.add('active');
+}
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из 
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event 
+ */
+function changeText(event) {
+    switch (event.target.textContent) {
+        case "Link 1":
+            title.textContent = texts.text1;
+            break;
+        case "Link 2":
+            title.textContent = texts.text2;
+            break;
+        case "Link 3":
+            title.textContent = texts.text3;
+            break;
+    }
+}
